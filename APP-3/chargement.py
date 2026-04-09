3e251d6caf7e8e6ae54a218ebdf993efe00708ae
guillaume bastide <guillaume1.bastide@esme.fr>
2026-04-09 09:20:29 +0200

    creation de lecteur de csv et recuperation des info

--- APP-3/chargement.py
+++ APP-3/chargement.py
@@ -1,1 +1,7 @@
 # Lecture CSV + strucure de données 
+import csv
+
+with open("APP-3/800.csv", "r", encoding="utf-8") as f:
+    lecteur = csv.DictReader(f)
+    for ligne in lecteur:
+        print(ligne)
