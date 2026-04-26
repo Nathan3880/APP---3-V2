d6a09b02c901afbc064c7f0d042114e12d1fe52c
TomMiguel33 <tom.miguel@esme.fr>
2026-04-26 11:51:23 +0200

    ajout fonction lire_csv_candidats

--- APP-3/chargement.py
+++ APP-3/chargement.py
@@ -2,7 +2,18 @@
 import csv
 import os
 
-def lire_csv(nom_fichier):
+def lire_csv_formations(nom_fichier):
+    formations = []
+    chemin = os.path.join(os.path.dirname(__file__), nom_fichier)
+    with open(chemin, "r", encoding="utf-8") as f:
+        lecteur = csv.DictReader(f)
+        for ligne in lecteur:
+            formations.append(ligne)
+    return formations
+
+print(lire_csv_formations("800.csv"))
+
+def lire_csv_candidats(nom_fichier):
     candidats = []
     chemin = os.path.join(os.path.dirname(__file__), nom_fichier)
     with open(chemin, "r", encoding="utf-8") as f:
@@ -11,4 +22,4 @@ def lire_csv(nom_fichier):
             candidats.append(ligne)
     return candidats
 
-print(lire_csv("800.csv"))
+print(lire_csv_candidats("parcoursup_small_10000.csv"))
