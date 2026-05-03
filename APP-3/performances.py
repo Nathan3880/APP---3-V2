ec00a7cc7f8d8fe2055fcb80580980de11415eb3
TomMiguel33 <tom.miguel@esme.fr>
2026-05-03 12:00:23 +0200

    Ajout fonctions performances

--- APP-3/performances.py
+++ APP-3/performances.py
@@ -1,1 +1,13 @@
 # Mesure des temps de tri sur gros volumes
+from timeit import*
+from tri import*
+from chargement import*
+
+def performances(liste, tri):
+    t1 = default_timer()
+    tri(liste)
+    t2 = default_timer()
+    return f"Le tri a besoin de {t2-t1} secondes pour trier les candidats"
+
+print(performances(lire_csv_formations("parcoursup_small_10000.csv"), tri_bulles))
+
--- APP-3/tri.py
+++ APP-3/tri.py
@@ -9,22 +9,22 @@ def tri_insertion(liste):
         j = i - 1
         while j >= 0 and arr[j]["score"] < en_cours["score"]:
             arr[j + 1] = arr[j]
-            j -= 1
+            j = j -1
         arr[j + 1] = en_cours
  
     return arr
  
 
- def tri_bulles(liste):
-
+def tri_bulles(liste):
     arr = liste[:]
     n = len(arr)
- 
+
     for i in range(n):
         for j in range(0, n - i - 1):
             if arr[j]["score"] < arr[j + 1]["score"]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
  
     return arr
+
 
 # Je suis pas sûr que ça marche mais j'ai adapté les codes de tri du cours à notre cas, en triant les étudiants par score décroissant.
