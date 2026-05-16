155ec28b83841ba1fe41e9eb62029cb430beee73
vico646 <victor.fresneau@esme.fr>
2026-05-16 20:04:27 +0200

    Update policies.py

--- APP-3/policies.py
+++ APP-3/policies.py
@@ -1,1 +1,16 @@
-# Policy 1 et 2 
+def policy_1(candidats, capacite):
+    triés = sorted(candidats, key=lambda c: (-c['score'], c['timestamp'], c['id']))
+    return triés[:capacite]
+
+def policy_2(candidats, capacite):
+    triés = sorted(candidats, key=lambda c: (
+        -(c['score'] // 10) * 10,  
+        -c['boursier'],             
+        c['timestamp'],
+        c['id']
+    ))
+    return triés[:capacite]
+
+
+print("POLICY 1:", [c['id'] for c in policy_1(candidats, 2)])
+print("POLICY 2:", [c['id'] for c in policy_2(candidats, 2)])
