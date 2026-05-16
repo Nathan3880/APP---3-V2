a1291e00bc6232e9a897cbe921faa1f4e5b8dd26
TomMiguel33 <tom.miguel@esme.fr>
2026-05-16 14:58:47 +0200

    modif code performances

--- APP-3/performances.py
+++ APP-3/performances.py
@@ -2,12 +2,13 @@
 from timeit import*
 from tri import*
 from chargement import*
+from parcoursup import*
 
-def performances(liste, tri):
+def performances(id_program, tri):
     t1 = default_timer()
-    tri(liste)
+    parcoursup(id_program, tri)
     t2 = default_timer()
     return f"Le tri a besoin de {t2-t1} secondes pour trier les candidats"
 
-print(performances(lire_csv_candidats("parcoursup_small_10000.csv"), tri_insertion))
+print(performances("1", tri_fusion))
 
