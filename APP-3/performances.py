a8a4a2580d4f4979ca0d3704c83cd0fc18a57b47
TomMiguel33 <tom.miguel@esme.fr>
2026-05-16 20:25:29 +0200

    pas de modif

--- APP-3/performances.py
+++ APP-3/performances.py
@@ -4,11 +4,12 @@ from tri import*
 from chargement import*
 from parcoursup import*
 
+
 def performances(id_program, tri):
     t1 = default_timer()
     parcoursup(id_program, tri)
     t2 = default_timer()
     return f"Le tri a besoin de {t2-t1} secondes pour trier les candidats"
 
-print(performances("1", tri_fusion))
+print(performances("1", tri_rapide))
 
