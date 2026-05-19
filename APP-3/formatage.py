97dc28fda1e63f06102447f9617be84e3df10c02
TomMiguel33 <tom.miguel@esme.fr>
2026-05-19 20:12:11 +0200

    ajout de tous les fichiers csv

--- APP-3/formatage.py
+++ APP-3/formatage.py
@@ -3,14 +3,14 @@ from chargement import*
 
 
 def info_etudiant(id_etudiant):
-    for candidat in lire_csv_candidats("parcoursup_small_10000.csv"):
+    for candidat in lire_csv_candidats("parcoursup_massive_500000.csv"):
         if candidat["candidate_id"] == id_etudiant:
             return candidat
     return "Il n'existe aucun étudiant ayant cet identifiant"
     
 
 def info_program(id_program):
-    for program in lire_csv_formations("800.csv"):
+    for program in lire_csv_formations("parcoursup_programs_small_800.csv"):
         if program["program_id"] == id_program:
             return program
     return "Il n'existe aucune formation ayant cet identifiant"
@@ -19,7 +19,7 @@ def info_program(id_program):
 def candidature_program(id_program):
     compteur = 0
     liste_etudiant = []
-    for candidat in lire_csv_candidats("parcoursup_small_10000.csv"):
+    for candidat in lire_csv_candidats("parcoursup_massive_500000.csv"):
         if candidat["program_id"] == id_program:
             liste_etudiant.append(candidat)
             compteur += 1
--- APP-3/performances.py
+++ APP-3/performances.py
@@ -11,5 +11,5 @@ def performances(id_program, tri):
     t2 = default_timer()
     return f"Le tri a besoin de {t2-t1} secondes pour trier les candidats"
 
-print(performances("1", tri_rapide))
+print(performances("1", tri_fusion))
 
